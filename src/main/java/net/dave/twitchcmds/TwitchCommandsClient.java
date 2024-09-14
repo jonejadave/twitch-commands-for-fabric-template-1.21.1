37609249ed3aac80e5c028a46143bfdf7d86f8cf
dave jonj <z1979867@students.niu.edu>
2024-09-14 08:28:22 -0500

    Initial commit

---
+++ src/main/java/net/dave/twitchcmds/TwitchCommandsClient.java
@@ -0,0 +1,10 @@
+package net.dave.twitchcmds;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class TwitchCommandsClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
